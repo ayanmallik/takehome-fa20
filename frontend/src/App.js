c63d294ef32d6a04c0d8f1f01781ce8a54aa72bd
repl.it user <replituser@example.com>
2020-09-13 21:12:24 +0000

    Submit Hack4Impact Takehome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
         {id: 2, name: "Potbelly", rating: 6},
         {id: 3, name: "Noodles and Company", rating: 8},
       ],
-      count: 0
+      count: 0,
+      inputValue: ""
     }
   }
 
@@ -21,11 +22,32 @@ class App extends Component {
       <div className="App">
         <Counter count={this.state.count}/>
         <Instructions complete={true} />
+
+        <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
+        <button type="button" onClick={this.addRestaurant}>Submit</button>
+
+        <br></br>
+        <br></br>
+
         {this.state.restaurants.map(x => (
           <Restaurant id={x.id} name={x.name} rating={x.rating} />
         ))}
       </div>
     )
+  }
+
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    })
+  }
+
+  addRestaurant = () => {
+    this.setState((prevState) => ({
+    restaurants: [...prevState.restaurants, 
+    {id: prevState.restaurants.length + 1,
+    name: prevState.inputValue,
+    rating: 0}]}))
   }
 }
 
--- frontend/src/Counter.js
+++ frontend/src/Counter.js
@@ -11,7 +11,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        {this.state.count}
+        Rating: {this.state.count}
         
         <br>
         </br>
